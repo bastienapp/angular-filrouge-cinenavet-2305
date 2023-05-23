dea62830467e1ef95e0fcc21cc2e0fea08f73c20
bastienapp <29610890+bastienapp@users.noreply.github.com>
2023-05-23 15:11:37 +0200

    liveco1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movie-details/movie-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-details', // slug-case, kebad-case
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.css']
+})
+export class MovieDetailsComponent {
+
+}
